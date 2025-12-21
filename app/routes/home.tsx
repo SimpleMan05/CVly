ece3f990070991a7767f7e22a103da25560fca72
Soumyadeep <173267965+SimpleMan05@users.noreply.github.com>
2025-12-21 21:49:10 +0530

    feat:altered welcome page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,21 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVly" },
+    { name: "description", content: "Smart Feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className = "main-section">
+      <div className="page-heading">
+        <h1>Track your Applications & Resume Ratings</h1>
+        <h2>Review your Submissions and check AI-powered feedback</h2>
+
+      </div>
+
+    </section>
+
+  </main>
 }
